16664ca9d177abdc23e823f5a30955a5454e89d7
Joel Bugarini <joelbugarini@hotmail.com>
2015-06-28 19:05:34 -0700

    Let Bindings

--- baby.hs
+++ baby.hs
@@ -17,8 +17,8 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
-factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorial' :: Integer -> Integer
+factorial' n = product [1..n]
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
@@ -40,3 +40,98 @@ myLength :: [list] -> Int
 myLength list = length list
 
 --P5 Reverse a List
+myReverse :: [list] -> [list]
+myReverse list = reverse list
+
+--P6 Find out whether a list is a palindrome. 
+--A palindrome can be read forward or backward; e.g. (x a m a x).
+--isPalindrome :: [list] -> Bool
+--isPalindrome list = if list == reverse list 
+--                      then True
+--                      else False
+
+--Pattern Matching
+lucky :: (Integral a) => a -> String
+lucky 7 = "Jackpot"
+lucky x = "Out of luck babe!"
+
+--Factorial Pattern Matching
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+--List Pattern Matching
+headita :: [a] -> a
+headita [] = error "cant call headita on empty list, -.-"
+headita (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "List empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two element: " ++ show x ++ " and " ++show y
+tell (x:y:_) = "No mames : " ++ show x ++ " and " ++show y
+
+length'' :: (Num b) => [a] -> b  
+length'' [] = 0  
+length'' (_:xs) = 1 + length' xs 
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+--Just Patterns
+capital :: String -> String
+capital "" = "Empty string puto"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+--Guards
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+--Initials
+--initials :: String -> String -> String
+--initials firstname lastname =
+--    where (f:_) = firstname
+--          (l:_) = lastname
+
+--Max with guards
+max' :: (Ord a) => a -> a -> a
+max' a b 
+	| a > b = a 
+	| otherwise = b
+
+--Compare with Guards
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT 
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+--Very similar  WHERE = LET
+--WHERE - All the function can see the construct
+--LET - Is more local
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in sideArea + 2 * topArea
+
+--P7 Transform a list, possibly holding lists as elements into a 
+--'flat' list by replacing each list with its elements (recursively).
+
